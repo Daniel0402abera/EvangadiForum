822dd180043fc24e455a79b710f14a9da8d3b562
Daniel0402abera <37146428+Daniel0402abera@users.noreply.github.com>
2023-01-19 12:33:29 +0300

    first commit

---
+++ evangadi_forum/server.js
@@ -0,0 +1,36 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mysql = require("mysql");
+
+const pool = require('./server/config/database')
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT;
+const userRouter = require('./server/api/users/user.router');
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api/users", userRouter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
