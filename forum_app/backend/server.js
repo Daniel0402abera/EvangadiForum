488cd07dc4f59fccc4021df35934bc1833ceb2fa
Daniel Abera <dsichale@gmail.com>
2023-03-23 15:33:57 +0300

    test commit

--- forum_app/backend/server.js
+++ forum_app/backend/server.js
@@ -15,12 +15,14 @@ const app = express();
 app.use(express.json())
 
 
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
   })
 
 
+  
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
