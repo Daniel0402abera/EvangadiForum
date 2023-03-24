388bc98ed2a88c1cacb258edab1cc6db4e5c60fd
Daniel Abera <dsichale@gmail.com>
2023-03-24 23:14:21 +0300

    header

---
+++ forum_app/frontend/src/components/header/Header.jsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { makeStyles } from "@mui/styles";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import Hidden from "@mui/material/Hidden";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import Divider from "@mui/material/Divider";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Link from "@mui/material/Link";
+
+
+const navigationLinks = [
+  { name: "Home", href: "#" },
+  { name: "How it works", href: "#" },
+  { name: "SIGN IN", href: "#" },
+];
+
+const useStyles = makeStyles((theme) => ({
+  link: {
+    // marginRight: 20,
+    padding:20
+  },
+  img: {
+    marginRight: "auto",
+    color: "white",
+    backgroundColor: "red",
+    borderRadius: 0,
+    height: 30,
+    border: "2px solid gray",
+    borderLeft: "12px solid transparent",
+    borderRight: "12px solid transparent",
+  },
+}));
+
+export default function Header() {
+  const styles = useStyles();
+  const [open, setOpen] = useState(false);
+  return (
+    <AppBar position="sticky" color="default">
+      <Container maxWidth="md">
+        <Toolbar disableGutters>
+          <img className={styles.img}  alt='logo'></img>
+          
+          <Hidden smDown>
+            {navigationLinks.map((item) => (
+              <Link
+                className={styles.link}
+                color="textPrimary"
+                variant="button"
+                underline="none"
+                href={item.href}
+                key={item.name}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </Hidden>
+          
+          <Hidden smUp>
+            <IconButton onClick={() => setOpen(true)}>
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
+        </Toolbar>
+      </Container>
+      <SwipeableDrawer
+        anchor="right"
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+      >
+        <div
+          onClick={() => setOpen(false)}
+          onKeyPress={() => setOpen(false)}
+          role="button"
+          tabIndex={0}
+        >
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {navigationLinks.map((item) => (
+            <ListItem key={item.name}>
+              <Link
+                className={styles.link}
+                color="textPrimary"
+                variant="button"
+                underline="none"
+                href={item.href}
+              >
+                {item.name}
+              </Link>
+            </ListItem>
+          ))}
+        </List>
+      </SwipeableDrawer>
+    </AppBar>
+  );
+}
